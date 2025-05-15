7da180d552d1c87ff93442d680e6b37a43d5c533
Hidayattt24 <hidayat.22@mhs.usk.ac.id>
2025-05-15 09:57:35 +0700

    feat : menambahkan link navigasi pindah halaman beranda dan artikel di header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,8 +1,8 @@
-'use client';
-import { useState, useEffect } from 'react';
-import { FaShoppingBasket, FaHeart, FaUserCircle } from 'react-icons/fa';
-import { getCurrentUser } from '@/lib/auth'; // Adjust path to your auth file
-import { User } from '@/types'; // Adjust path to your types file
+"use client";
+import { useState, useEffect } from "react";
+import { FaShoppingBasket, FaHeart, FaUserCircle } from "react-icons/fa";
+import { getCurrentUser } from "@/lib/auth"; // Adjust path to your auth file
+import { User } from "@/types"; // Adjust path to your types file
 
 export default function Header() {
   const [user, setUser] = useState<User | null>(null);
@@ -15,14 +15,13 @@ export default function Header() {
         setUser(response.user ?? null); // Convert undefined to null
         setLoading(false);
       } catch (error) {
-        console.error('Failed to fetch user:', error);
+        console.error("Failed to fetch user:", error);
         setUser(null);
         setLoading(false);
-      
       }
     };
     fetchUser();
-  }, );
+  });
 
   return (
     <header className="flex items-center justify-between px-10 py-8 bg-transparent absolute top-0 left-0 right-0 z-10">
@@ -37,7 +36,7 @@ export default function Header() {
 
       {/* Navigation - Centered */}
       <nav className="bg-1 text-white px-8 py-3 rounded-full flex space-x-6 text-sm font-medium gap-4 absolute left-1/2 transform -translate-x-1/2">
-        <a href="#" className="hover:underline">
+        <a href="/" className="hover:underline">
           Beranda
         </a>
         <a href="/katalog" className="hover:underline">
@@ -46,7 +45,7 @@ export default function Header() {
         <a href="/konsultasi" className="hover:underline">
           Konsul Pakan
         </a>
-        <a href="#" className="hover:underline">
+        <a href="/artikel" className="hover:underline">
           Artikel & Tips
         </a>
       </nav>
