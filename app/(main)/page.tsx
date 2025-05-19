54b56659713b8b75f6bf9d4e6cee01cf6623dbc7
Muhammad Habil Aswad <habil_asw@mhs.usk.ac.id>
2025-05-20 02:24:48 +0700

    feat: chatbot

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -8,11 +8,13 @@ import SegeraHadir from "@/components/pages/home/SegeraHadir";
 import Konsultasi from "@/components/pages/home/Konsultasi";
 import LatestBlog from "@/components/pages/home/LatestBlog";
 import KenapaPilih from "@/components/pages/home/KenapaPililh";
+import ChatbotWidget from "@/components/ChatbotWidget";
 
 
 export default function Home() {
   return (
     <>
+      <ChatbotWidget />
       <Hero />
       <BestSeller />
       <SpecialOffer />
